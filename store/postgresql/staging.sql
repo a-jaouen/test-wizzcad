f6bcc0946912068d592cf2163cd3aaa7e4ed40ec
Antoine Jaouën <antoine@orohealth.me>
2022-10-12 17:28:39 +0200

    feat: add better seeds

--- store/postgresql/staging.sql
+++ store/postgresql/staging.sql
@@ -2,7 +2,7 @@
 -- pre-populate some data for tests or something alike
 
 INSERT INTO forms(uuid, archived, items )
-VALUES ( '8e691352-e0a4-4bf5-ad9b-3f8f4cbb5340', false, '[]');
+VALUES ( '8e691352-e0a4-4bf5-ad9b-3f8f4cbb5340', false, '[{"uuid":"67f18feb-f54b-40b2-b772-b210e82d0ca2","archived":false,"objectKind":"form","items":[{"objectKind":"section","items":[{"fieldKind":"string","objectKind":"field","name":"Name","required":true},{"fieldKind":"integer","objectKind":"field","name":"Age","required":true},{"fieldKind":"string","objectKind":"field","name":"Gender","required":false},{"objectKind":"section","items":[{"fieldKind":"boolean","objectKind":"field","name":"Number of children"}]}]}]}]');
 
 INSERT INTO forms(uuid, archived, items )
 VALUES ( 'a3298f23-8fb6-46c8-8038-6cbe66912b88', true, '[]');
